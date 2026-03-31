8d853952c75eaecda02c6bd8f3e335b5bc203288
wanfeng_zjg <a3136178263@126.com>
2026-03-31 15:47:04 +0800

    chapter01 learning. use dashscope as LLM, but deepseek is also ok.

--- m01_agent_introduction/agent-demo.py
+++ m01_agent_introduction/agent-demo.py
@@ -3,13 +3,19 @@ from config import OPENAI_API_KEY
 from langchain_openai import ChatOpenAI
 
 # 配置deepseek
+# llm = ChatOpenAI(
+#     model="deepseek-chat",
+#     api_key=OPENAI_API_KEY,
+#     # 注:在.env里把OPENAI_API_KEY改成你自己的api-key即可
+#     base_url="https://api.deepseek.com"
+# )
+# 配置千问
 llm = ChatOpenAI(
-    model="deepseek-chat",
+    model="qwen3.5-plus",
     api_key=OPENAI_API_KEY,
-    # 注:在.env里把OPENAI_API_KEY改成你自己的api-key即可
-    base_url="https://api.deepseek.com"
+    base_url="https://dashscope.aliyuncs.com/compatible-mode/v1"
 )
 
 # 调用模型
-response = llm.invoke("你好呀")
+response = llm.invoke("你好呀,当前你用的是什么大模型呢")
 print(response.content)
